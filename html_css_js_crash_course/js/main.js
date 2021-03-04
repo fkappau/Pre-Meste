5e6b2ea5cf8916b0db3032ba73ae731b421bd420
fkappau <fkappau@gmail.com>
2021-03-04 04:57:44 -0800

    add arrays and objects

--- html_css_js_crash_course/js/main.js
+++ html_css_js_crash_course/js/main.js
@@ -1,5 +1,32 @@
-console.log("helloooo .....")
+let a=9
+let b=10
+console.log(a+b)
+console.log(a*b)
+let surname="Apau"
 
-let b = 78
+let Firstname="Francis"
+console.log(surname+ " " +Firstname)
 
-console.log(b);
+//arrays and objects
+
+let names = ["kofi","ama","kwaku"]
+let mymixd = [1,4,6, "ama","kwaku"]
+let myarray = [22,23,43,2,33,2,2]
+
+console.log(myarray[5]);
+myarray.push(50)
+console.log(myarray)
+console.log(mymixd[3])
+
+//objects 
+//always have key value text
+//eg 
+let myobject={
+    name:"kofi",
+    class:3
+    ,classname:"mrygold",
+    results[
+        {subject:"English",mark:40,position:1}
+        {subject:"math",mark:50,position:5}
+        {}
+    ]
